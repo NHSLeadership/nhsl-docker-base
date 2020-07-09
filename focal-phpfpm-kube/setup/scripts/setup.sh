c95576548148bcaa799a1dee95adb310a437320b
Tawmu <tom.ferguson@outlook.com>
2020-07-09 21:30:44 +0100

    fix atatus setup

--- focal-phpfpm-kube/setup/scripts/setup.sh
+++ focal-phpfpm-kube/setup/scripts/setup.sh
@@ -22,11 +22,11 @@ sed -i -e "s|;sendmail_path =|sendmail_path = /usr/sbin/ssmtp -t|g" /nhsla/etc/p
 #####
 if [ ! -z "$ATATUS_APM_LICENSE_KEY" ]; then
   # If API key set then configure Atatus
-  sed -i -e "s/atatus.license_key = \"\"/atatus.license_key = \"$ATATUS_APM_LICENSE_KEY\"/g" /nhsla/etc/20-atatus.ini
-  sed -i -e "s/atatus.release_stage = \"production\"/atatus.release_stage = \"$ENVIRONMENT\"/g" /nhsla/etc/20-atatus.ini
-  sed -i -e "s/atatus.app_name = \"PHP App\"/atatus.app_name = \"$SITE_NAME\"/g" /nhsla/etc/20-atatus.ini
-  sed -i -e "s/atatus.app_version = \"\"/atatus.app_version = \"$SITE_BRANCH-$BUILD\"/g" /nhsla/etc/20-atatus.ini
-  sed -i -e "s/atatus.tags = \"\"/atatus.tags = \"$SITE_BRANCH-$BUILD, $SITE_BRANCH\"/g" /nhsla/etc/20-atatus.ini
+  sed -i -e "s/atatus.license_key = \"\"/atatus.license_key = \"$ATATUS_APM_LICENSE_KEY\"/g" /nhsla/etc/atatus.ini
+  sed -i -e "s/atatus.release_stage = \"production\"/atatus.release_stage = \"$ENVIRONMENT\"/g" /nhsla/etc/atatus.ini
+  sed -i -e "s/atatus.app_name = \"PHP App\"/atatus.app_name = \"$SITE_NAME\"/g" /nhsla/etc/atatus.ini
+  sed -i -e "s/atatus.app_version = \"\"/atatus.app_version = \"$SITE_BRANCH-$BUILD\"/g" /nhsla/etc/atatus.ini
+  sed -i -e "s/atatus.tags = \"\"/atatus.tags = \"$SITE_BRANCH-$BUILD, $SITE_BRANCH\"/g" /nhsla/etc/atatus.ini
   printf " %-30s %-30s\n" "Atatus:" "Enabled"
 else
   # Atatus - if api key is not set then disable
