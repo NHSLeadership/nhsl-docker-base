19fa87b181718d2df6fcc90ce94dce60ec29e32a
Tawmu <tom.ferguson@outlook.com>
2021-06-01 23:13:41 +0100

    Openresty cleanup and move to s6-overlay

---
+++ openresty/root/etc/cont-init.d/01-boot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+with-contenv
+
+# Create directories if they don't exist otherwise container wont run.
+
+[[ -d /nhsla/etc ]] || mkdir -p /nhsla/etc
+mkdir -p /tmp/openresty
+
+if [ ! -z "$AWS_HOST_ENVIRONMENT" ]; then
+  cp -rp /app/. /app-shared/
+fi
