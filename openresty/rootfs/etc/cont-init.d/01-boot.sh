1cfbcb0aab4709d7febc358afc7a421fc19ebc4a
Tawmu <tom.ferguson@outlook.com>
2022-09-01 13:06:50 +0100

    remove redundant app to app-shared copy in Openresty image

--- openresty/rootfs/etc/cont-init.d/01-boot.sh
+++ openresty/rootfs/etc/cont-init.d/01-boot.sh
@@ -5,10 +5,7 @@ with-contenv
 # Create directories if they don't exist otherwise container wont run.
 mkdir -p /tmp/openresty
 
+# Copy over default config if we dont already have one from elsewhere
 if [ ! -f /usr/local/openresty/nginx/conf/site.conf ]; then
   mv /usr/local/openresty/nginx/conf/site.conf.default /usr/local/openresty/nginx/conf/site.conf
-fi
-
-if [ ! -z "$AWS_HOST_ENVIRONMENT" ]; then
-  cp -rp /app/. /app-shared/
 fi
